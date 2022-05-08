dd5f95299593ce2c7c727a6964ba8a259047b43a
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2022-05-08 19:16:06 +0800

    updated build.sh as a proper script

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
- zip install.zip common/* META-INF/* system/etc/security/cacerts customize.sh LICENSE module.prop post-fs-data.sh README.md uninstall.sh
- git add install.zip update.json changelog.md module.prop
+#!/bin/bash
+rm -f ./install.zip
+zip install.zip common/* META-INF/* system/etc/security/cacerts/* customize.sh LICENSE module.prop post-fs-data.sh README.md uninstall.sh
+git add install.zip update.json changelog.md module.prop
