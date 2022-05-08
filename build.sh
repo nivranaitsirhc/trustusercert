67af2235e15fb0f5cb2a4130e4276392821cf96b
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2022-05-08 19:42:45 +0800

    fix build.sh install.zip archive not including META-INF/*

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -f ./install.zip
-zip install.zip common/* META-INF/* system/etc/security/cacerts/* customize.sh LICENSE module.prop post-fs-data.sh README.md uninstall.sh
+zip install.zip common/* META-INF/com/google/android/* system/etc/security/cacerts/* customize.sh LICENSE module.prop post-fs-data.sh README.md uninstall.sh
 git add install.zip update.json changelog.md module.prop
